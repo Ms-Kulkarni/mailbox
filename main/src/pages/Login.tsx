f3f4eb28f21207d6b4cfadfa68ad926d93fe992a
M s kulkarni <109647254+Ms-Kulkarni@users.noreply.github.com>
2024-08-10 23:24:56 +0530

    Update Login.tsx

--- main/src/pages/Login.tsx
+++ main/src/pages/Login.tsx
@@ -14,7 +14,7 @@ function Login() {
   }
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://fontend.com/";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://mailbox-ytpq.vercel.app/";
   };
 
   return (
