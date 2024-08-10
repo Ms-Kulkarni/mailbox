5fb3f74a04ee29c583a280b302bb06c4ba9113e9
M s kulkarni <109647254+Ms-Kulkarni@users.noreply.github.com>
2024-08-10 23:08:30 +0530

    Add files via upload

---
+++ main/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <div className="bg-[#121212] flex justify-center items-center text-sm bottom-0 fixed w-screen h-8 text-[#5C5F66]">
+      All rights reserved. © 2024 Reachinbox.
+    </div>
+  )
+}
+
+export default Footer
